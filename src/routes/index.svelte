12135058ff5a8798a974b94b57d62c000710ac13
sinYa Iwasaki <iwasaki@sinproject.net>
2022-04-29 21:53:31 +0900

    メイン画面 画像を表示する #8

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import porkKatsuImage from '$lib/assets/images/a9426cb9-5950-4cde-a011-80672a2a7295.jpeg'
+
+  console.log(porkKatsuImage)
+</script>
+
+<img src={porkKatsuImage} alt="" />
+
+<style>
+  :global(body) {
+    margin: 0;
+  }
+</style>
