74d831ce88d11d20eca1e482fae88979efaa56d6
sinYa Iwasaki <iwasaki@sinproject.net>
2022-11-23 18:55:29 +0900

    コードフォーマットを更新する #14

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,86 @@
 module.exports = {
-	root: true,
-	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-	plugins: ['svelte3', '@typescript-eslint'],
-	ignorePatterns: ['*.cjs'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-	settings: {
-		'svelte3/typescript': () => require('typescript')
-	},
-	parserOptions: {
-		sourceType: 'module',
-		ecmaVersion: 2020
-	},
-	env: {
-		browser: true,
-		es2017: true,
-		node: true
-	}
-};
+  root: true,
+  parser: '@typescript-eslint/parser',
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  plugins: ['svelte3', '@typescript-eslint'],
+  ignorePatterns: ['*.cjs'],
+  overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+  settings: {
+    'svelte3/typescript': () => require('typescript'),
+  },
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2020,
+  },
+  env: {
+    browser: true,
+    es2017: true,
+    node: true,
+  },
+  rules: {
+    '@typescript-eslint/explicit-member-accessibility': ['error'],
+    '@typescript-eslint/explicit-function-return-type': ['warn'],
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'typeParameter',
+        format: ['UPPER_CASE'],
+      },
+      {
+        selector: ['class', 'interface', 'typeAlias'],
+        format: ['PascalCase'],
+      },
+      {
+        selector: ['method', 'function'],
+        modifiers: ['private'],
+        format: ['snake_case'],
+        leadingUnderscore: 'require',
+      },
+      {
+        selector: ['method', 'function'],
+        modifiers: ['protected'],
+        format: ['snake_case'],
+        leadingUnderscore: 'require',
+      },
+      {
+        selector: ['method', 'function'],
+        format: ['snake_case'],
+      },
+      {
+        selector: [
+          'property',
+          'accessor',
+          'parameter',
+          'parameterProperty',
+          'variable',
+          'enumMember',
+        ],
+        modifiers: ['private'],
+        format: ['snake_case'],
+        leadingUnderscore: 'require',
+      },
+      {
+        selector: [
+          'property',
+          'accessor',
+          'parameter',
+          'parameterProperty',
+          'variable',
+          'enumMember',
+        ],
+        modifiers: ['protected'],
+        format: ['snake_case'],
+        leadingUnderscore: 'require',
+      },
+      {
+        selector: ['accessor', 'parameter', 'parameterProperty', 'enumMember'],
+        format: ['snake_case'],
+      },
+      {
+        selector: ['property', 'variable'],
+        format: ['snake_case', 'UPPER_CASE', 'camelCase'],
+      },
+      // TODO: 引数、関数、クラス、インターフェイス、enumのメンバー名のルール
+    ],
+  },
+}
